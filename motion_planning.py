22912e209a2c4fefbd6bc1ae39ac37bce5ce9323
obees <olivierbourrasse@hotmail.com>
2018-03-26 14:52:26 +0200

    graph version

--- motion_planning.py
+++ motion_planning.py
@@ -1,16 +1,19 @@
 import argparse
 import time
 import msgpack
-from enum import Enum, auto
-
 import numpy as np
-
-from planning_utils import a_star, heuristic, create_grid
+import networkx as nx
+import numpy.linalg as LA
+from enum import Enum, auto
+from planning_utils import a_star_graph, heuristic, Sampler
 from udacidrone import Drone
 from udacidrone.connection import MavlinkConnection
 from udacidrone.messaging import MsgID
 from udacidrone.frame_utils import global_to_local
-
+from udacidrone.frame_utils import local_to_global
+from shapely.geometry import Polygon, Point, LineString
+from queue import PriorityQueue
+from sklearn.neighbors import KDTree
 
 class States(Enum):
     MANUAL = auto()
@@ -80,6 +83,7 @@ class MotionPlanning(Drone):
     def takeoff_transition(self):
         self.flight_state = States.TAKEOFF
         print("takeoff transition")
+        print('self.target_position[2]'.format(self.target_position[2]))
         self.takeoff(self.target_position[2])
 
     def waypoint_transition(self):
@@ -108,57 +112,130 @@ class MotionPlanning(Drone):
 
     def send_waypoints(self):
         print("Sending waypoints to simulator ...")
+        print('self.waypoints:{}'.format(self.waypoints))
         data = msgpack.dumps(self.waypoints)
         self.connection._master.write(data)
+
+    def can_connect(self,n1, n2, polygons):
+        l = LineString([n1, n2])
+        for p in polygons:
+            if p.crosses(l) and p.height >= min(n1[2], n2[2]):
+                return False
+        return True
+
+    def create_graph(self,nodes, k, polygons):
+        g = nx.Graph()
+        tree = KDTree(nodes)
+        for n1 in nodes:
+            # for each node connect try to connect to k nearest nodes
+            idxs = tree.query([n1], k, return_distance=False)[0]
+
+            for idx in idxs:
+                n2 = nodes[idx]
+                if n2 == n1:
+                    continue
+
+                if self.can_connect(n1, n2, polygons):
+                    heuristic_val = heuristic(n1,n2)
+                    g.add_edge(n1, n2, weight=heuristic_val)
+        return g
+
+    def prune_path(self, path, polygons):
+
+        pruned_path = [p for p in path]
+        # TODO: prune the path!
+
+        i = 0
+        while i < len(pruned_path)-2:
+            p1 = pruned_path[i]
+            j=i+2
+            connected_j = 0
+            while j < len(pruned_path):
+                p3 = pruned_path[j]
+                if self.can_connect(p1, p3, polygons):
+                    connected_j = j
+                j = j + 1
+            if connected_j > 0:
+                for k in range(i+1, connected_j):
+                    pruned_path.remove(pruned_path[i+1])
+            i = i + 1
+
+        return pruned_path
 
     def plan_path(self):
         self.flight_state = States.PLANNING
         print("Searching for a path ...")
-        TARGET_ALTITUDE = 5
+        TARGET_ALTITUDE = 19
         SAFETY_DISTANCE = 5
 
-        self.target_position[2] = TARGET_ALTITUDE
-
         # TODO: read lat0, lon0 from colliders into floating point values
-        
+        with open('colliders.csv') as f:
+            [lat0,lon0] = [float(a.lstrip(' ').split(' ')[1]) for a in f.readline().rstrip('\n').split(',')]
+
         # TODO: set home position to (lat0, lon0, 0)
+        self.set_home_position(lon0, lat0, 0)
 
         # TODO: retrieve current global position
- 
+        global_position = (self._longitude, self._latitude, self._altitude)
+
         # TODO: convert to current local position using global_to_local()
-        
+        current_local_pos = global_to_local(global_position, self.global_home)
+
         print('global home {0}, position {1}, local position {2}'.format(self.global_home, self.global_position,
                                                                          self.local_position))
+
         # Read in obstacle map
         data = np.loadtxt('colliders.csv', delimiter=',', dtype='Float64', skiprows=2)
-        
-        # Define a grid for a particular altitude and safety margin around obstacles
-        grid, north_offset, east_offset = create_grid(data, TARGET_ALTITUDE, SAFETY_DISTANCE)
-        print("North offset = {0}, east offset = {1}".format(north_offset, east_offset))
-        # Define starting point on the grid (this is just grid center)
-        grid_start = (-north_offset, -east_offset)
-        # TODO: convert start position to current position rather than map center
-        
-        # Set goal as some arbitrary position on the grid
-        grid_goal = (-north_offset + 10, -east_offset + 10)
-        # TODO: adapt to set goal as latitude / longitude position and convert
 
-        # Run A* to find a path from start to goal
+        # TODO: convert start position to current position rather than map center
+        grid_start = (int(current_local_pos[0]),int(current_local_pos[1]),int(current_local_pos[2]))
+
+        # TODO: adapt to set goal as latitude / longitude position and convert
+        grid_goal_global = (-122.398921, 37.797879, -20)
+        grid_goal_to_local = global_to_local(grid_goal_global, self.global_home)
+        grid_goal = (int(grid_goal_to_local[0]),int(grid_goal_to_local[1]),int(grid_goal_to_local[2]))
+
+
         # TODO: add diagonal motions with a cost of sqrt(2) to your A* implementation
         # or move to a different search space such as a graph (not done here)
+
         print('Local Start and Goal: ', grid_start, grid_goal)
-        path, _ = a_star(grid, heuristic, grid_start, grid_goal)
-        
+
+        # Using graph approach
+        # Sampling environment initialization with obstacles data
+        # sampling class using a k-d tree adapted to support TARGET_ALTITUDE, SAFETY_DISTANCE, grid_start, grid_goal
+        sampler = Sampler(data, TARGET_ALTITUDE, SAFETY_DISTANCE)
+
+        # Retrieve polygons objects list representing the obstacles
+        # Giving access to polygon.contains and polygon.crosses methods
+        polygons = sampler._polygons
+
+        # sampling 200 points and removing
+        # ones conflicting with obstacles.
+        # Adding grid_start and grid_goal allows to verify against obstacle collision early
+        nodes = sampler.sample(300, grid_start, grid_goal)
+        #print(nodes)
+        # create graph from possible states, obstacle polygons, and edges to 10 closest states
+        g = self.create_graph(nodes, 10, polygons)
+        #print(g.nodes)
+        #Search a path using graph adapted A* algorithm
+        path, cost = a_star_graph(g, heuristic, grid_start, grid_goal)
+
         # TODO: prune path to minimize number of waypoints
         # TODO (if you're feeling ambitious): Try a different approach altogether!
+        path = self.prune_path(path, polygons)
 
-        # Convert path to waypoints
-        waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in path]
+        # Convert path to waypoints skipping grid_start
+        waypoints = [[int(p[0]), int(p[1]), int(p[2]), 0] for p in path[1:]]
+
         # Set self.waypoints
         self.waypoints = waypoints
         # TODO: send waypoints to sim
-        self.send_waypoints()
-
+        # Abort mission if path to goal not found
+        if len(path)>0:
+            self.send_waypoints()
+        else:
+            self.disarming_transition()
     def start(self):
         self.start_log("Logs", "NavLog.txt")
 
--- planning_utils.py
+++ planning_utils.py
@@ -1,101 +1,17 @@
 from enum import Enum
 from queue import PriorityQueue
 import numpy as np
+import math
+from sklearn.neighbors import KDTree
+from shapely.geometry import Polygon, Point
 
+def heuristic(position, goal_position):
+    return np.linalg.norm(np.array(position) - np.array(goal_position))
 
-def create_grid(data, drone_altitude, safety_distance):
-    """
-    Returns a grid representation of a 2D configuration space
-    based on given obstacle data, drone altitude and safety distance
-    arguments.
-    """
-
-    # minimum and maximum north coordinates
-    north_min = np.floor(np.min(data[:, 0] - data[:, 3]))
-    north_max = np.ceil(np.max(data[:, 0] + data[:, 3]))
-
-    # minimum and maximum east coordinates
-    east_min = np.floor(np.min(data[:, 1] - data[:, 4]))
-    east_max = np.ceil(np.max(data[:, 1] + data[:, 4]))
-
-    # given the minimum and maximum coordinates we can
-    # calculate the size of the grid.
-    north_size = int(np.ceil((north_max - north_min + 1)))
-    east_size = int(np.ceil((east_max - east_min + 1)))
-
-    # Initialize an empty grid
-    grid = np.zeros((north_size, east_size))
-
-    # Populate the grid with obstacles
-    for i in range(data.shape[0]):
-        north, east, alt, d_north, d_east, d_alt = data[i, :]
-        if alt + d_alt + safety_distance > drone_altitude:
-            obstacle = [
-                int(np.clip(north - d_north - safety_distance - north_min, 0, north_size-1)),
-                int(np.clip(north + d_north + safety_distance - north_min, 0, north_size-1)),
-                int(np.clip(east - d_east - safety_distance - east_min, 0, east_size-1)),
-                int(np.clip(east + d_east + safety_distance - east_min, 0, east_size-1)),
-            ]
-            grid[obstacle[0]:obstacle[1]+1, obstacle[2]:obstacle[3]+1] = 1
-
-    return grid, int(north_min), int(east_min)
-
-
-# Assume all actions cost the same.
-class Action(Enum):
-    """
-    An action is represented by a 3 element tuple.
-
-    The first 2 values are the delta of the action relative
-    to the current grid position. The third and final value
-    is the cost of performing the action.
-    """
-
-    WEST = (0, -1, 1)
-    EAST = (0, 1, 1)
-    NORTH = (-1, 0, 1)
-    SOUTH = (1, 0, 1)
-
-    @property
-    def cost(self):
-        return self.value[2]
-
-    @property
-    def delta(self):
-        return (self.value[0], self.value[1])
-
-
-def valid_actions(grid, current_node):
-    """
-    Returns a list of valid actions given a grid and current node.
-    """
-    valid_actions = list(Action)
-    n, m = grid.shape[0] - 1, grid.shape[1] - 1
-    x, y = current_node
-
-    # check if the node is off the grid or
-    # it's an obstacle
-
-    if x - 1 < 0 or grid[x - 1, y] == 1:
-        valid_actions.remove(Action.NORTH)
-    if x + 1 > n or grid[x + 1, y] == 1:
-        valid_actions.remove(Action.SOUTH)
-    if y - 1 < 0 or grid[x, y - 1] == 1:
-        valid_actions.remove(Action.WEST)
-    if y + 1 > m or grid[x, y + 1] == 1:
-        valid_actions.remove(Action.EAST)
-
-    return valid_actions
-
-
-def a_star(grid, h, start, goal):
-    """
-    Given a grid and heuristic function returns
-    the lowest cost path from start to goal.
-    """
+# A* for graphs
+def a_star_graph(graph, heuristic, start, goal):
 
     path = []
-    path_cost = 0
     queue = PriorityQueue()
     queue.put((0, start))
     visited = set(start)
@@ -112,33 +28,114 @@ def a_star(grid, h, start, goal):
             print('Found a path.')
             found = True
             break
+
         else:
-            # Get the new vertexes connected to the current vertex
-            for a in valid_actions(grid, current_node):
-                next_node = (current_node[0] + a.delta[0], current_node[1] + a.delta[1])
-                new_cost = current_cost + a.cost + h(next_node, goal)
+            for next_node in graph[current_node]:
+                cost = graph.edges[current_node,next_node]["weight"]
+                new_cost = current_cost + cost + heuristic(next_node, goal)
 
                 if next_node not in visited:
                     visited.add(next_node)
                     queue.put((new_cost, next_node))
 
-                    branch[next_node] = (new_cost, current_node, a)
+                    branch[next_node] = (new_cost, current_node)
 
+    path = []
+    path_cost = 0
     if found:
+
         # retrace steps
+        path = []
         n = goal
         path_cost = branch[n][0]
-        path.append(goal)
+        path.append(n)
         while branch[n][1] != start:
             path.append(branch[n][1])
             n = branch[n][1]
         path.append(branch[n][1])
-    else:
-        print('**********************')
-        print('Failed to find a path!')
-        print('**********************') 
     return path[::-1], path_cost
 
-def heuristic(position, goal_position):
-    return np.linalg.norm(np.array(position) - np.array(goal_position))
+class Poly:
 
+    def __init__(self, coords, height):
+        self._polygon = Polygon(coords)
+        self._height = height
+
+    @property
+    def height(self):
+        return self._height
+
+    @property
+    def coords(self):
+        return list(self._polygon.exterior.coords)[:-1]
+
+    @property
+    def area(self):
+        return self._polygon.area
+
+    @property
+    def center(self):
+        return (self._polygon.centroid.x, self._polygon.centroid.y)
+
+    def contains(self, point):
+        point = Point(point)
+        return self._polygon.contains(point)
+
+    def crosses(self, other):
+        return self._polygon.crosses(other)
+
+
+
+def extract_polygons(data, safety_distance):
+
+    polygons = []
+    marge = safety_distance
+    for i in range(data.shape[0]):
+        north, east, alt, d_north, d_east, d_alt = data[i, :]
+
+        obstacle = [int(north - d_north - marge), int(north + d_north + marge), int(east - d_east - marge), int(east + d_east + marge)]
+        corners = [(obstacle[0], obstacle[2]), (obstacle[0], obstacle[3]), (obstacle[1], obstacle[3]), (obstacle[1], obstacle[2])]
+
+        # TODO: Compute the height of the polygon
+        height = alt + d_alt + marge
+
+        p = Poly(corners, height)
+        polygons.append(p)
+
+    return polygons
+class Sampler:
+
+    def __init__(self, data, target_altitude, safety_distance):
+        self._polygons = extract_polygons(data, safety_distance)
+        self._xmin = np.min(data[:, 0] - data[:, 3])
+        self._xmax = np.max(data[:, 0] + data[:, 3])
+
+        self._ymin = np.min(data[:, 1] - data[:, 4])
+        self._ymax = np.max(data[:, 1] + data[:, 4])
+
+        self._zmin = target_altitude - 1
+        # limit z-axis
+        self._zmax = target_altitude + 1
+
+        centers = np.array([p.center for p in self._polygons])
+        self._tree = KDTree(centers, metric='euclidean')
+
+    def sample(self, num_samples, grid_start, grid_goal):
+        """Implemented with a k-d tree for efficiency."""
+        xvals = np.random.uniform(self._xmin, self._xmax, num_samples)
+        yvals = np.random.uniform(self._ymin, self._ymax, num_samples)
+        zvals = np.random.uniform(self._zmin, self._zmax, num_samples)
+        samples = list(zip(xvals, yvals, zvals))
+        samples.insert(0,grid_start)
+        samples.append(grid_goal)
+        pts = []
+        for s in samples:
+            _, idx = self._tree.query(np.array([s[0], s[1]]).reshape(1, -1))
+            p = self._polygons[int(idx)]
+            if not p.contains(s) or p.height < s[2]:
+                pts.append(s)
+        return pts
+
+    @property
+    def polygons(self):
+        return self._polygons
